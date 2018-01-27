56502a21841a2732b014f41416c1fe8aa9c87eeb
Francesco Perrotti-Garcia <fpg1503@gmail.com>
2018-01-26 23:26:36 -0200

    Remove host from server

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const http = require('http')
 const port = process.env.port || 3000
 
-http.createServer(function (req, res) {
+const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'})
   res.end('Hello World\n')
-}).listen(port, 'localhost')
+})
+
+server.listen(port)
 console.log(`Server running at http://localhost:${port}/`)
