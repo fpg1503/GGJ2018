3b0b00d7bf6d2c8a9fd0b14151646172f23ecd7b
Francesco Perrotti-Garcia <fpg1503@gmail.com>
2018-01-27 00:05:41 -0200

    Move index back to its own folder

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const http = require('http')
-const port = process.env.port || 3000
+const port = process.env.port || 80
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'})
