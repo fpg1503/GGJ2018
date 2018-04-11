963f75d5537ed26f368584e799210097038283a4
Francesco Perrotti-Garcia <fpg1503@gmail.com>
2018-04-10 23:58:28 -0300

    Add ranking and completion status

--- server/Model/Level.js
+++ server/Model/Level.js
@@ -8,7 +8,11 @@ const LevelSchema = new Schema({
     name: { type: String, required: true },
     map: { type: [{x: Number, y: Number, type: {type: String}, _id: false}], required: true },
     lastEditedBy: { type: String, required: true },
-    lastEditedAt: { type: String, required: true }
+    creatorName: { type: String, required: true },
+    lastEditedAt: { type: String, required: true },
+    attempts: { type: [{ userId: String, successful: Boolean, _id: false}], required: true},
+    completed: { type: Boolean, required: true, default: false},
+    numberOfAttempts: { type: Number, required: true, default: 0}
 })
 
 const model = mongoose.model('Level', LevelSchema)
--- server/index.js
+++ server/index.js
@@ -23,9 +23,9 @@ app.get('/levels', (req, res) => {
   ])
 })
 
-app.get('/levels/:id', (req, res) => {
+app.get('/levels/:number/random', (req, res) => {
   Level.findOne(
-    { level: { $eq: req.params.id }, lastEditedBy: { $ne: req.query.userName }},
+    { level: { $eq: req.params.number }, lastEditedBy: { $ne: req.query.userName }, completed: { $ne: true }, 'map.0': {$exists: true}},
     { __v: false },
     {sort: { 'lastEditedAt': -1 }})
   .then(result => {
@@ -34,15 +34,28 @@ app.get('/levels/:id', (req, res) => {
   .catch(error => console.error(error))
 })
 
-app.post('/levels/:id', (req, res) => {
+app.get('/levels/:number/ranking', (req, res) => {
+  Level.find( { level: { $eq: req.params.number } }, { creatorName: 1, numberOfAttempts: 1 }).sort({ numberOfAttempts: -1 }).limit(req.query.pageSize || 10)  
+  .then(result => {
+    const mapped = result.map ( values => {
+      return { name: values.creatorName, ranking: values.numberOfAttempts}
+    })
+    res.json(mapped)  
+  })
+  .catch(error => console.error(error))
+})
+
+app.post('/levels/:number/creation', (req, res) => {
   console.log(req.body)
   const level = new Level({
     name: 'First',
-    level: req.params.id,
+    level: req.params.number,
     map: req.body.map,
     parentId: req.body.parentId,
-    lastEditedBy: req.body.userName,
-    lastEditedAt: new Date()
+    lastEditedBy: req.body.userId,
+    creatorName: req.body.name,
+    lastEditedAt: new Date(),
+    attempts: []
   })
   level.save()
   .then(console.log)
@@ -50,5 +63,20 @@ app.post('/levels/:id', (req, res) => {
   res.statusCode = 201
   res.send()
 })
+
+app.post('/levels/:number/version/:versionId/attempt', (req, res) => {
+  const completed = req.body.completed
+  Level.findOneAndUpdate({ level: { $eq: req.params.number }, _id: { $eq: req.params.versionId} }, { $inc: { numberOfAttempts: 1 }, $set: { completed }, $push: { attempts: { userId: req.body.user, successful: completed } } })
+  .then(result => {
+    console.log(result)
+    res.statusCode = 201
+    res.send()
+  })
+  .catch(error => {
+    console.error(error)
+    res.error(error)
+  })
+})
+
 
 app.listen(port, () => console.log(`Listening on port ${port}!`))
